39edf2ab17fad9701832c8b3161b425ffbae66ce
zTopMan <42548224+zTopMan@users.noreply.github.com>
2019-01-27 13:18:52 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Vasco-Pixel/mz/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/yTopMan/FireForce/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
