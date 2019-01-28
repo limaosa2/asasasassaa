e6810a7c15e7a69b28a31b8e1dbce10ae2f695db
zTopMan <42548224+zTopMan@users.noreply.github.com>
2019-01-28 20:30:11 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -55,7 +55,7 @@ window.addEventListener('load', function () {
     var div = document.createElement('div');
     div.setAttribute('class', 'post block bc2');
     div.innerHTML = '<style>.grecaptcha-badge{display: none;}</style>   <div id="minimapbg" style="position: absolute; right: 1em; bottom: 1em;">' +
-'<div class="posy" id="posyt" style="background-size: 100%; background-image: url(https://imgur.com/8rmVmFp); color: rgb(255, 255, 255); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 12px; padding: 10px;">' +
+'<div class="posy" id="posyt" style="background-size: 100%; background-image: url(https://prnt.sc/mdhymd); color: rgb(255, 255, 255); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 12px; padding: 10px;">' +
         '<div id="minimap-text" style="display: none;"></div>' +
         '<div id="minimap-box" style="position: relative;width:420px;height:300px">' +
         '<canvas id="minimap" style="width: 100%; height: 100%;z-index:1;position:absolute;top:0;left:0;"></canvas>' +
